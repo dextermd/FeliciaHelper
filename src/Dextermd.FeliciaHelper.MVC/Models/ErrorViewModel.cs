656cf9ec2880b82e1504853d3869f52698b83214
DEXTER MD <dextermdpay@gmail.com>
2022-11-21 23:26:52 +0200

    Add project files.

---
+++ src/Dextermd.FeliciaHelper.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dextermd.FeliciaHelper.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
